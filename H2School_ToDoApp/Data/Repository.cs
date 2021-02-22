76c62a62aec70f5032f22e154795541cb6889487
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-02-22 19:48:09 +0100

    Making an Interface with the different mechanics needed for our CRUD to our Lists of To Do's.

---
+++ H2School_ToDoApp/Data/Repository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using H2School_ToDoApp.Data;
+using H2School_ToDoApp.Models;
+
+namespace H2School_ToDoApp.Data
+{
+    public class Repository : IRepository
+    {
+        private List<ToDo> todos;
+
+        public Repository()
+        {
+            todos = new List<ToDo>
+            {
+                new ToDo{ Id=1, Title="Shopping List", Description="Listing down what I need to buy at the store.", DeadLine=2}
+            };
+        }
+
+        /// <summary>
+        /// Creating a new To Do List.
+        /// </summary>
+        /// <param name="newTodo"></param>
+        public void Create(ToDo newTodo)
+        {
+            newTodo.Id = todos.Max(t => t.Id);
+            ++newTodo.Id;
+            todos.Add(newTodo);
+        }
+
+        /// <summary>
+        /// Updating / Editing a To Do List.
+        /// </summary>
+        /// <param name="newTodo"></param>
+        public void Update(ToDo newTodo)
+        {
+            int index = todos.FindIndex(t => t.Id == newTodo.Id);
+            todos[index].Title = newTodo.Title;
+        }
+
+        /// <summary>
+        /// Getting all of the To Do Lists.
+        /// </summary>
+        /// <returns></returns>
+        public List<ToDo> GetAll()
+        {
+            return todos;
+        }
+
+        /// <summary>
+        /// Finding a To Do List by its Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ToDo GetItemById(int? id)
+        {
+            return todos.SingleOrDefault(t => t.Id == id);
+        }
+
+        /// <summary>
+        /// Removing a To Do from the List.
+        /// </summary>
+        /// <param name="id"></param>
+        public void Remove(int id)
+        {
+            todos.Remove(GetItemById(id));
+        }
+    }
+}
